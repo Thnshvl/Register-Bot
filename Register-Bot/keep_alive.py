ab545766ebe765436bbc80e38a7640b260c00976
Thnshvl <thaneshvelgunaseelan@gmail.com>
2025-05-19 01:30:54 +0800

    Update keep_alive.py
    wrong print meathod

--- Register-Bot/keep_alive.py
+++ Register-Bot/keep_alive.py
@@ -12,6 +12,6 @@ def run():
     app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-    print("✅ Hosting at:", f"https://{os.environ['REPL_SLUG']}.{os.environ['REPL_OWNER']}.repl.co")
+    print("✅ Flask server is running on Render!")
     t = Thread(target=run)
     t.start()
